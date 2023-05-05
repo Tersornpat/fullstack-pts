ecb176a7470828d4806db4b5c106b78197782ef2
Tersornpat <sornpat@gmail.com>
2023-05-05 13:21:42 +0700

    clean

--- connectdb.php
+++ connectdb.php
@@ -10,13 +10,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// $showdata = "SELECT * FROM M_Member";
-// $result = $conn->query($showdata);
-// $conn->close();
-
-// while($row = $result->fetch_assoc()) {
-//     echo $row["Member_ID"]."   ".$row["Member_Name"]."   ".$row["Member_Surname"]."   ".$row["Member_Address"]."   ".$row["Member_Tel"]."<br>";
-// }
-
 ?>
